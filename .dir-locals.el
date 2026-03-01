4501f379803fcc986876648837fef5bbb9e9ebb1
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-03-01 15:40:06 -0500

    Update

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;; pre-commit-elisp .dir-locals.el - https://github.com/jamescherti/pre-commit-elisp
-((nil . ((pre-commit-elisp-load-path . ("home/.emacs-use-package.d/lisp/local"
+((nil . ((pre-commit-elisp-load-path . ("home/.emacs-use-package.d/lisp/local/modules/"
+                                        "home/.emacs-use-package.d/lisp/local/"
                                         "../../emacs/lightemacs"))
          (pre-commit-elisp-error-on-compile-warning . t))))
