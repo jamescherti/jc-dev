80cd4d8579d35073ecd83093dbb25f046850b08a
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-03-11 23:30:41 -0400

    Update

--- home/.emacs-use-package.d/lisp/local/modules/mod-misc.el
+++ home/.emacs-use-package.d/lisp/local/modules/mod-misc.el
@@ -1498,35 +1498,27 @@ Returns:
 
 ;;; C-g
 
-;; NOTE: Issue. When I press C-g while selecting text, it moves the cursor
-(defun my/keyboard-quit-dwim ()
-  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
-
-The generic `keyboard-quit' does not do the expected thing when
-the minibuffer is open.  Whereas we want it to close the
-minibuffer, even without explicitly focusing it.
-
-The DWIM behaviour of this command is as follows:
-
-- When the region is active, disable it.
-- When a minibuffer is open, but not focused, close the minibuffer.
-- When the Completions buffer is selected, close it.
-- In every other case use the regular `keyboard-quit'."
-  (interactive)
-  (cond
-   ((region-active-p)
-    (keyboard-quit))
-   ((derived-mode-p 'completion-list-mode)
-    (delete-completion-window))
-   ((and (minibufferp)
-         (> (minibuffer-depth) 0))
-    ;; Fixes vertico: Not in most nested command loop (Evil insert mode)
-    (abort-recursive-edit))
-   (t
-    (keyboard-quit))))
-
-(define-key global-map (kbd "C-g") #'my/keyboard-quit-dwim)
-
+;; (defun my/keyboard-quit-dwim ()
+;;   "Do-What-I-Mean behaviour for a general 'keyboard-quit'.
+;;
+;; The DWIM behaviour of this command is as follows:
+;;
+;; - When the region is active, disable it.
+;; - When the Completions buffer is selected, close it.
+;; - When a minibuffer is active (even if not focused), abort it.
+;; - In every other case use the regular 'keyboard-quit'."
+;;   (interactive)
+;;   (cond
+;;    ((region-active-p)
+;;     (keyboard-quit))
+;;    ((derived-mode-p 'completion-list-mode)
+;;     (delete-completion-window))
+;;    ((active-minibuffer-window)
+;;     (abort-recursive-edit))
+;;    (t
+;;     (keyboard-quit))))
+;;
+;; (define-key global-map (kbd "C-g") #'my/keyboard-quit-dwim)
 
 ;;; Setup scratch
 
