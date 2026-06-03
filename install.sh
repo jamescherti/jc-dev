57d8a95ceceee8ec6f276bccbe049018be408048
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-06-03 12:27:52 -0400

    Update

--- install.sh
+++ install.sh
@@ -27,7 +27,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #
-# --------------------------------------------------TEST
 
 # shellcheck disable=SC2269
 set -euf -o pipefail
