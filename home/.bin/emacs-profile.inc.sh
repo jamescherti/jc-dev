ed48f4004b55e792b72c3e84413091cd5b98d89f
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-06-27 20:47:28 -0400

    Update

--- home/.bin/emacs-profile.inc.sh
+++ home/.bin/emacs-profile.inc.sh
@@ -4,6 +4,14 @@ RUN_EMACS_BIN="${RUN_EMACS_BIN:-}"
 OSFAMILY=$(~/.local/bin/osid 2>/dev/null || echo "unknown")
 
 if ! [[ -f "$RUN_EMACS_BIN" ]]; then
+  export \
+    RUN_EMACS_BIN="/opt/local/$USER/$OSFAMILY/emacs/branch-master/bin/emacs"
+  if [[ $EMACS_D = "" ]]; then
+    EMACS_D="$HOME/.emacs-master.d/"
+  fi
+fi
+
+if ! [[ -f "$RUN_EMACS_BIN" ]]; then
   # export \
   #   RUN_EMACS_BIN="/opt/local/$USER/$OSFAMILY/emacs/branch-master/bin/emacs"
   export RUN_EMACS_BIN="/opt/local/$USER/$OSFAMILY/emacs/branch-emacs-31/bin/emacs"
@@ -26,14 +34,6 @@ fi
 #    EMACS_D="$HOME/.emacs-use-package.d/"
 #  fi
 #fi
-
-if ! [[ -f "$RUN_EMACS_BIN" ]]; then
-  export \
-    RUN_EMACS_BIN="/opt/local/$USER/$OSFAMILY/emacs/branch-master/bin/emacs"
-  if [[ $EMACS_D = "" ]]; then
-    EMACS_D="$HOME/.emacs-master.d/"
-  fi
-fi
 
 echo "[INFO] Emacs binary: $RUN_EMACS_BIN"
 if ! [[ -f "$RUN_EMACS_BIN" ]]; then
