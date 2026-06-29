8cebe595ababf2745255a52d6474586e5d0f9389
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-06-29 16:35:20 -0400

    Update

--- home/.bin/emacs-profile.inc.sh
+++ home/.bin/emacs-profile.inc.sh
@@ -15,6 +15,14 @@ if ! [[ -f "$RUN_EMACS_BIN" ]]; then
   export \
     RUN_EMACS_BIN="/opt/local/$USER/$OSFAMILY/emacs/branch-master/bin/emacs"
   if [[ $EMACS_D = "" ]]; then
+    EMACS_D="$HOME/.emacs-straight.d/"
+  fi
+fi
+
+if ! [[ -f "$RUN_EMACS_BIN" ]]; then
+  export \
+    RUN_EMACS_BIN="/opt/local/$USER/$OSFAMILY/emacs/branch-master/bin/emacs"
+  if [[ $EMACS_D = "" ]]; then
     EMACS_D="$HOME/.emacs-master.d/"
   fi
 fi
