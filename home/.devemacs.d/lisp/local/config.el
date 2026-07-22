a1daa21b09d5b7ed1cadd3cc32bb010ea6fd6db1
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-07-22 11:46:32 -0400

    Update

--- home/.devemacs.d/lisp/local/config.el
+++ home/.devemacs.d/lisp/local/config.el
@@ -1,9 +1,4 @@
 ;;; config.el --- config -*- no-byte-compile: t; lexical-binding: t -*-
-;;
-
-;;; Commentary:
-
-;;; Code:
 
 ;; Author: James Cherti
 ;; URL: https://github.com/jamescherti/jc-dev
@@ -30,37 +25,11 @@
 ;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ;; SOFTWARE.
 
-;; (defun my-load-from-elc-cache-advice (orig-fun file &optional noerror nomessage nosuffix must-suffixes)
-;;   "Advice to load .elc from `my-elc-cache-directory' if available and newer."
-;;   (let* ((found-file (if (file-name-absolute-p file)
-;;                          file
-;;                        (locate-file file load-path (unless nosuffix (get-load-suffixes)))))
-;;          (cached-elc (when found-file
-;;                        (let ((rel (replace-regexp-in-string "^/" "" (expand-file-name found-file))))
-;;                          (expand-file-name (concat (file-name-sans-extension rel) ".elc") my-elc-cache-directory)))))
-;;     (if (and cached-elc
-;;              (file-exists-p cached-elc)
-;;              (file-newer-than-file-p cached-elc found-file))
-;;         ;; Load the cached .elc file
-;;         (apply orig-fun cached-elc noerror nomessage nosuffix must-suffixes)
-;;       ;; Fallback to standard loading
-;;       (apply orig-fun file noerror nomessage nosuffix must-suffixes))))
+;;; Commentary:
 
-;; Advise `load' to check the cache directory first
-;; (advice-add 'load :around #'my-load-from-elc-cache-advice)
-
-;;; Add some paths (copy paste)
-(require 'lightemacs)
-;; (add-to-list 'load-path (expand-file-name "modules/"
-;;                                           lightemacs-local-directory))
-;; TOTO replace them with push
-;; (add-to-list 'load-path lightemacs-local-modules-directory)
-;; (add-to-list 'load-path lightemacs-core-directory)
-;; (add-to-list 'load-path lightemacs-modules-directory)
+;;; Code:
 
 ;;; External config
-
-;; (message "LOAD: config.el")
 
 (load (expand-file-name "~/.config.el") :no-error :no-message :nosuffix)
 
@@ -77,53 +46,43 @@
   ;; Prevent Emacs from writing custom settings to any file
   (advice-add 'custom-save-all :override #'ignore))
 
-;;; Ido
-
-;; (setq ido-everywhere t
-;;       ido-enable-last-directory-history t
-;;       ido-max-work-directory-list 30
-;;       ido-max-work-file-list 50
-;;       ido-max-prospects 8
-;;       ido-confirm-unique-completion nil
-;;       ido-create-new-buffer 'always
-;;       ido-use-virtual-buffers t)
-;; (add-hook 'lightemacs-after-init-hook #'ido-mode)
-
-;;; Emacs
-
-(setq lightemacs-load-compiled-init-files t)
+;;; Native-compilation
 
 (setq enable-dir-local-variables t)
 (setq enable-local-variables :safe)
 
 (setq native-comp-jit-compilation t)
-(setq native-comp-deferred-compilation nil)
-(setq package-native-compile nil)
+(setq native-comp-deferred-compilation t)
+(setq package-native-compile t)
+
+;;; Config
 
 ;; (setq lightemacs-theme-default-font "Iosevka Medium")
 ;; (setq lightemacs-theme-default-font "Iosevka SemiBold")
 (setq lightemacs-theme-default-font "Iosevka Term SemiBold")
-(setq lightemacs-saveplace-recenter-after-find-file t)
 
-;;; Lightemacs
+(setq lightemacs-load-compiled-init-files t)
+(setq lightemacs-saveplace-recenter-after-find-file t)
 
 (setq minimal-emacs-frame-title-format "Devemacs")
 
 (setq lightemacs-dired-omit-parent-directory t)
 (setq lightemacs-cycle nil)
+(setq lightemacs-theme-name 'tomorrow-night-deepblue)
+(setq lightemacs-theme-package 'tomorrow-night-deepblue-theme)
 
 (setq lightemacs-modules '(le-flavor-micro
                            le-pathaction
                            le-maybe-markdown-ts
-                           le-org
                            le-group-evil
                            le-persist-text-scale
-                           le-consult
-                           le-vertico
                            le-xclip
 
                            le-compile-angel
 
+                           ;; le-org
+                           ;; le-consult
+                           ;; le-vertico
                            ;; le-autorevert
                            ;; le-undo-fu
                            ;; le-undo-fu-session
@@ -138,45 +97,17 @@
                            ;; le-git-modes
                            ))
 
-(setq lightemacs-theme-name 'tomorrow-night-deepblue)
-(setq lightemacs-theme-package 'tomorrow-night-deepblue-theme)
+;;; Ido
 
-;;; Changes to evil-collection
-
-;; This snippet safely overrides the default evil-collection keybindings for
-;; diff-mode by delaying execution until after the package loads. It
-;; specifically targets the 'motion' state map and unbinds the 'd' key by
-;; assigning it a nil value, which cancels out the 'diff-hunk-kill' command.
-;; This ensures the 'd' key reverts to its standard Evil behavior for deletion
-;; operations, preventing accidental deletions of diff hunks while you navigate.
-
-;; (add-hook 'diff-mode-hook
-;;           (lambda ()
-;;             (evil-define-key 'motion 'local (kbd "d") nil)
-;;             (evil-define-key 'motion 'local (kbd "D") nil)
-;;             )
-;;           t)
-
-;; (with-eval-after-load 'evil-collection
-;;   (add-hook 'evil-collection-setup-hook
-;;             (lambda (mode &rest _rest)
-;;               (when (eq mode 'diff-mode)
-;;                 (evil-define-key 'motion diff-mode-map "d" nil)
-;;                 (evil-define-key 'motion diff-mode-map "D" nil)))))
-
-
-;; (defun lightemacs-user-early-init ()
-;;   "Run this after early-init."
-;;   (message "[INIT] user pre-early-init")
-;;   )
-
-;; (defun lightemacs-user-init ()
-;;   "Run this after init."
-;;   (message "[INIT] user init"))
-
-;; (defun lightemacs-user-post-early-init ()
-;;   "Run this after early-init."
-;;   (message "[INIT] user early init"))
+(setq ido-everywhere t
+      ido-enable-last-directory-history t
+      ido-max-work-directory-list 30
+      ido-max-work-file-list 50
+      ido-max-prospects 8
+      ido-confirm-unique-completion nil
+      ido-create-new-buffer 'always
+      ido-use-virtual-buffers t)
+(add-hook 'lightemacs-after-init-hook #'ido-mode)
 
 ;;; DISABLED: Simple Commit message
 
