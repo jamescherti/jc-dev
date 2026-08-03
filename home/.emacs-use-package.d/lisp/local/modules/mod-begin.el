c7295b2f42d7ae3f47dd74c2a2b4e180df1ff414
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-08-03 13:50:42 -0400

    Update

--- home/.emacs-use-package.d/lisp/local/modules/mod-begin.el
+++ home/.emacs-use-package.d/lisp/local/modules/mod-begin.el
@@ -37,7 +37,23 @@
 (setq lightemacs-electric-pair-local-target-hooks nil)
 (setq lightemacs-electric-pair-global-target-hooks nil)
 (add-hook-text-editing-modes #'electric-pair-local-mode)
+
+;; I don't like it in the minibuffer, especially when searching for
+;; things
 (add-hook 'minibuffer-setup-hook 'electric-pair-local-mode)
+
+(defun my-disable-electric-pair-in-evil (orig-fn &rest args)
+  "Temporarily remove `electric-pair-local-mode' from minibuffer setup.
+ORIG-FN and ARGS are the original function and its arguments."
+  (let ((minibuffer-setup-hook (remove 'electric-pair-local-mode minibuffer-setup-hook)))
+    (apply orig-fn args)))
+(with-eval-after-load 'evil-commands
+  (advice-add 'evil-ex-search-forward :around #'my-disable-electric-pair-in-evil)
+  (advice-add 'evil-ex-search-backward :around #'my-disable-electric-pair-in-evil)
+  (advice-add 'evil-search-forward :around #'my-disable-electric-pair-in-evil)
+  (advice-add 'evil-search-backward :around #'my-disable-electric-pair-in-evil))
+(with-eval-after-load 'evil-ex
+  (advice-add 'evil-ex :around #'my-disable-electric-pair-in-evil))
 
 (with-eval-after-load 'le-evil-snipe
   (setq lightemacs-evil-snipe-local-target-hooks nil)
