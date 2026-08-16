44566e1f7ab4aebddcee798c1b9f5350794665c9
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-08-16 17:47:37 -0400

    Update

--- home/.emacs-use-package.d/lisp/local/modules/mod-dired.el
+++ home/.emacs-use-package.d/lisp/local/modules/mod-dired.el
@@ -69,110 +69,106 @@
                                            dired-filter-by-git-ignored
                                            dired-filter-by-dot-files))
 
-;;; Other dired settings
+;;; Open dired with external command
 
-(defun my-dired-get-file-open-command (file-path)
-  "Return FILE-PATH corresponding command from `dired-guess-shell-alist-user'."
-  (let* ((case-fold-search nil)
-         (result (seq-find (lambda (pattern)
-                             (string-match-p (car pattern) file-path))
-                           dired-guess-shell-alist-user)))
-    (when result
-      (car (cdr result)))))
+;; (defun my-dired-get-file-open-command (file-path)
+;;   "Return FILE-PATH corresponding command from `dired-guess-shell-alist-user'."
+;;   (let* ((case-fold-search nil)
+;;          (result (seq-find (lambda (pattern)
+;;                              (string-match-p (car pattern) file-path))
+;;                            dired-guess-shell-alist-user)))
+;;     (when result
+;;       (car (cdr result)))))
+;;
+;; (defun my-dired-open-with-external-command ()
+;;   "Open the current file in `dired' using an external command based on file type."
+;;   (interactive nil dired-mode)
+;;   (if (and (fboundp 'dired-get-file-for-visit)
+;;            (fboundp 'dired--find-possibly-alternative-file))
+;;       (let* ((file (dired-get-file-for-visit)))
+;;         (let ((shell-cmd (my-dired-get-file-open-command file)))
+;;           (if shell-cmd
+;;               (progn
+;;                 ;; (message "[RUN] %s %s" shell-cmd file)
+;;                 (if (fboundp 'quick-fasd-add-path)
+;;                     (quick-fasd-add-path file)
+;;                   (message "Warning: Undefined: `quick-fasd-add-path'"))
+;;                 (call-process shell-cmd nil nil nil file))
+;;             (condition-case err
+;;                 (dired--find-possibly-alternative-file file)
+;;               (error
+;;                (message "Container parsing failed (%s). Opening literally."
+;;                         (error-message-string err))
+;;                (find-file-literally file))))))
+;;     (error
+;;      "Undefined: dired-get-file-for-visit or dired--find-possibly-alternative-file")))
+;;
+;; (defun my-dired-xdg-open ()
+;;   "Open the file under the cursor using xdg-open."
+;;   (interactive)
+;;   (if (fboundp 'dired-get-file-for-visit)
+;;       (let* ((file (dired-get-file-for-visit)))
+;;         (when my-dired-xdg-open-cmd
+;;           (call-process my-dired-xdg-open-cmd nil nil nil file)))
+;;     (error "Undefined: dired-get-file-for-visit")))
+;;
+;; (with-eval-after-load 'dired
+;;   ;; --------------------------------------------------------------------------
+;;   ;; Using xdg-open/open/start for certain filetypes
+;;   ;; --------------------------------------------------------------------------
+;;   (defvar my-dired-xdg-open-cmd nil)
+;;
+;;   (when-let* ((cmd (cond (IS-MAC "open")
+;;                          (IS-LINUX "xdg-open")
+;;                          (IS-WINDOWS "start"))))
+;;     (when cmd
+;;       (setq my-dired-xdg-open-cmd cmd)))
+;;
+;;   (setq dired-guess-shell-alist-user
+;;         `(("\\.\\(?:docx\\|pdf\\|odt\\|odg\\|ods\\|djvu\\|eps\\)\\'" ,cmd)
+;;           ("\\.\\(?:jpe?g\\|webp\\|png\\|gif\\|xpm\\)\\'" ,cmd)
+;;           ("\\.\\(?:xcf\\)\\'" ,cmd)
+;;           ("\\.tex\\'" ,cmd)
+;;           ("\\.\\(?:mp4\\|mkv\\|m4a\\|avi\\|flv\\|rm\\|rmvb\\|ogv\\)\\(?:\\.part\\)?\\'" ,cmd)
+;;           ("\\.\\(?:mp3\\|flac\\)\\'" ,cmd)))
+;;
+;;   ;; Advise `dired-find-file' to use `my-dired-open-with-external-command'
+;;   ;; instead
+;;   (advice-add 'dired-find-file :override #'my-dired-open-with-external-command))
 
-(defun my-dired-open-with-external-command ()
-  "Open the current file in `dired' using an external command based on file type."
-  (interactive nil dired-mode)
-  (if (and (fboundp 'dired-get-file-for-visit)
-           (fboundp 'dired--find-possibly-alternative-file))
-      (let* ((file (dired-get-file-for-visit)))
-        (let ((shell-cmd (my-dired-get-file-open-command file)))
-          (if shell-cmd
-              (progn
-                ;; (message "[RUN] %s %s" shell-cmd file)
-                (if (fboundp 'quick-fasd-add-path)
-                    (quick-fasd-add-path file)
-                  (message "Warning: Undefined: `quick-fasd-add-path'"))
-                (call-process shell-cmd nil nil nil file))
-            (condition-case err
-                (dired--find-possibly-alternative-file file)
-              (error
-               (message "Container parsing failed (%s). Opening literally."
-                        (error-message-string err))
-               (find-file-literally file))))))
-    (error
-     "Undefined: dired-get-file-for-visit or dired--find-possibly-alternative-file")))
+;;; Abbreviate dired headers
 
-(with-eval-after-load 'dired
+;; --------------------------------------------------------------------------
+;; Abbreviate dired header
+;; https://emacs.stackexchange.com/questions/33799/is-there-any-way-to-abbreviate-dired-header
+;;
+;; I modified it to make it only modify the first line
+;;
+;; NOTE: does not work. it sometimes changes where the directory is
+;; --------------------------------------------------------------------------
+;; TODO: Contribution to Emacs?
+;; (defvar my-dired-abbreviate-header t)
+;;
+;; (defun my-dired-readin-abbreviate-header (&rest _)
+;;   "Abbreviate home directory path to '~' in the first line of the buffer."
+;;   (when my-dired-abbreviate-header
+;;     (save-excursion
+;;       (goto-char (point-min))
+;;       (let ((inhibit-read-only t)
+;;             (case-fold-search nil)
+;;             (home (expand-file-name "~"))
+;;             (line-end (line-end-position)))
+;;         (while (search-forward home line-end t)
+;;           (replace-match "~" t t))))))
+;;
+;; (advice-add 'dired-readin :after 'my-dired-readin-abbreviate-header)
 
-  ;; --------------------------------------------------------------------------
-  ;; Functions
-  ;; --------------------------------------------------------------------------
-  (defun my-dired-home ()
-    "Dired home."
-    (interactive)
-    (dired "~/"))
+;;; Misc
 
-  ;; --------------------------------------------------------------------------
-  ;; Abbreviate dired header
-  ;; https://emacs.stackexchange.com/questions/33799/is-there-any-way-to-abbreviate-dired-header
-  ;;
-  ;; I modified it to make it only modify the first line
-  ;;
-  ;; NOTE: does not work. it sometimes changes where the directory is
-  ;; --------------------------------------------------------------------------
-  ;; TODO: Contribution to Emacs?
-  ;; (defvar dired-abbreviate-header t)
-  ;;
-  ;; (defun my-dired-readin-abbreviate-header (&rest _)
-  ;;   "Abbreviate home directory path to '~' in the first line of the buffer."
-  ;;   (when dired-abbreviate-header
-  ;;     (save-excursion
-  ;;       (goto-char (point-min))
-  ;;       (let ((inhibit-read-only t)
-  ;;             (case-fold-search nil)
-  ;;             (home (expand-file-name "~"))
-  ;;             (line-end (line-end-position)))
-  ;;         (while (search-forward home line-end t)
-  ;;           (replace-match "~" t t))))))
-  ;;
-  ;; (advice-add 'dired-readin :after 'my-dired-readin-abbreviate-header)
-
-  ;; --------------------------------------------------------------------------
-  ;; Using xdg-open/open/start for certain filetypes
-  ;; --------------------------------------------------------------------------
-  (defvar my-dired-xdg-open-cmd nil)
-
-  (defun my-dired-xdg-open ()
-    "Make Dired open the file under the cursor."
-    (interactive)
-    ;; Removed: (dired-get-filename nil t)
-    (if (fboundp 'dired-get-file-for-visit)
-        (let* ((file (dired-get-file-for-visit)))
-          (when my-dired-xdg-open-cmd
-            (call-process my-dired-xdg-open-cmd nil nil nil file)))
-      (error "Undefined: dired-get-file-for-visit")))
-
-  (when-let* ((cmd (cond (IS-MAC "open")
-                         (IS-LINUX "xdg-open")
-                         (IS-WINDOWS "start"))))
-    (when cmd
-      (setq my-dired-xdg-open-cmd cmd)
-      (setq dired-guess-shell-alist-user
-            `(("\\.\\(?:docx\\|pdf\\|odt\\|odg\\|ods\\|djvu\\|eps\\)\\'" ,cmd)
-              ("\\.\\(?:jpe?g\\|webp\\|png\\|gif\\|xpm\\)\\'" ,cmd)
-              ("\\.\\(?:xcf\\)\\'" ,cmd)
-              ("\\.tex\\'" ,cmd)
-              ("\\.\\(?:mp4\\|mkv\\|m4a\\|avi\\|flv\\|rm\\|rmvb\\|ogv\\)\\(?:\\.part\\)?\\'" ,cmd)
-              ("\\.\\(?:mp3\\|flac\\)\\'" ,cmd)
-              ;; ("\\.csv\\'" ,cmd)
-              ;; ("\\.html?\\'" ,cmd)
-              ;; ("\\.md\\'" ,cmd)
-              ))))
-
-  ;; Advise `dired-find-file' to use `my-dired-open-with-external-command'
-  ;; instead
-  (advice-add 'dired-find-file :override #'my-dired-open-with-external-command))
+(defun my-dired-home ()
+  "Dired home."
+  (interactive)
+  (dired "~/"))
 
 ;;; Icons dired
 
