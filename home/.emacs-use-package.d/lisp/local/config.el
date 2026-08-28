b7f4ce8b616d1d8f29ee2157fbe0ed290182552d
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-08-28 17:55:22 -0400

    Update

--- home/.emacs-use-package.d/lisp/local/config.el
+++ home/.emacs-use-package.d/lisp/local/config.el
@@ -1547,11 +1547,11 @@ Iterates over `my-package-base-directory' and adds all subdirectories to
 
 (defun lightemacs-user-before-modules ()
   "Pre-modules."
-  (require 'benchmark-init)
-  (require 'benchmark-init-modes)
-  (when (fboundp 'benchmark-init/activate)
-    (benchmark-init/activate)
-    (add-hook 'window-setup-hook 'benchmark-init/deactivate 90))
+  ;; (require 'benchmark-init)
+  ;; (require 'benchmark-init-modes)
+  ;; (when (fboundp 'benchmark-init/activate)
+  ;;   (benchmark-init/activate)
+  ;;   (add-hook 'window-setup-hook 'benchmark-init/deactivate 90))
 
   (my-add-packages-to-load-path))
 
