967245c86255d9eec093915f347174a73509bf03
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-09-12 13:47:47 -0400

    Fix Emacs loading issues and scrolling

--- home/.emacs-use-package.d/lisp/local/modules/mod-misc.el
+++ home/.emacs-use-package.d/lisp/local/modules/mod-misc.el
@@ -238,7 +238,7 @@ When non-nil, `tab-width' is updated automatically when a major mode loads.")
       ;;
       ;; Large jumps (> 9 lines off-screen): Emacs gives up on scrolling and
       ;; recenters the window.
-      scroll-conservatively 8
+      ;; scroll-conservatively 8
 
       ;; Setting scroll-conservatively to most-positive-fixnum: By default, when
       ;; your cursor moves off the screen, Emacs recenters the window so the
@@ -250,7 +250,7 @@ When non-nil, `tab-width' is updated automatically when a major mode loads.")
       ;;
       ;; Setting scroll-step to 1: This variable tells Emacs to scroll by
       ;; exactly one line at a time when the cursor moves off-screen.
-      ;; scroll-conservatively most-positive-fixnum
+      scroll-conservatively most-positive-fixnum
 
       ;; In modern Emacs, if using scroll-conservatively, scroll-step is
       ;; completely redundant. Setting scroll-step 1 is a legacy holdover from
