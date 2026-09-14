d81c5715973c6a19ea5a53dd43f819ce15aa45f6
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-09-14 11:20:12 -0400

    Remove Python config

--- home/.emacs-use-package.d/lisp/local/modules/mod-misc.el
+++ home/.emacs-use-package.d/lisp/local/modules/mod-misc.el
@@ -3506,9 +3506,10 @@ WINDOW-OR-FRAME is provided by `window-buffer-change-functions'."
 (setq eat-minimum-latency 0.01)
 (setq eat-maximum-latency 0.05)
 
-(setq vterm-max-scrollback 500)
+;;; TODO
+;; (setq vterm-max-scrollback 500)
 (setq ghostel-max-scrollback (* 1024 1024)
-      (setq eat-term-scrollback-size (* 64 1024)))
+      eat-term-scrollback-size (* 64 1024))
 
 ;; Disable shell prompt status annotations in the window margin. Prevents Eat
 ;; from displaying exit code indicators (such as the default "0" or "X") beside
