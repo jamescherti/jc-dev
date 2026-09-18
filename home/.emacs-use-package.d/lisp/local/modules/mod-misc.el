c35e28b2754b50d8788e69fc81e8f117154d7d69
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-09-18 13:46:45 -0400

    Update

--- home/.emacs-use-package.d/lisp/local/modules/mod-misc.el
+++ home/.emacs-use-package.d/lisp/local/modules/mod-misc.el
@@ -222,7 +222,7 @@ When non-nil, `tab-width' is updated automatically when a major mode loads.")
                     ((= gc-cons-threshold most-positive-fixnum) "∞")
                     (t (format "%sM" (/ gc-cons-threshold 1000000))))))
 
-(setq-default mode-line-position '(" %l:%c (%p)"))
+(setq-default mode-line-position '("%l:%c  |  %p"))
 
 (unless noninteractive
   (setq display-time-mail-function #'ignore
